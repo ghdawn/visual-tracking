172eb752d55d841d36ea45f5f5b8adfd9b618cad
ghdawn <ghdawn31@gmail.com>
2014-02-26 12:01:53 +0800

    创建了项目框架

---
+++ CradleControl/cradlecontrol.h
@@ -0,0 +1,39 @@
+#ifndef CRADLECONTROL_H_INCLUDED
+#define CRADLECONTROL_H_INCLUDED
+#include "itrbase.h"
+#include "itrvision.h"
+using itr_math::Vector;
+
+/**
+* \brief 提供云台控制的功能
+* \note 需要串口支持
+*/
+class Cradle
+{
+        /**
+        * \brief 初始化控制参数
+        * \param degX 水平方向视场夹角(单位:角度)
+        * \param degY 垂直方向向视场夹角(单位:角度)
+        * \param u0 水平半幅图像的像素数(单位:像素)
+        * \param v0 垂直半幅图像的像素数(单位:像素)
+        */
+        void Init(F32 degX,F32 degY,F32 u0,F32 v0);
+
+        /**
+        * \brief 利用跟踪结果控制云台
+        * \param 向量X为系统的状态向量
+        * \param X[0] x方向位置
+        * \param X[1] y方向位置
+        * \param X[2] x方向速度
+        * \param X[3] y方向速度
+        */
+        void Control(Vector X);
+
+        /**
+        * \brief 直接控制云台转速
+        * \param omegax x方向角速率
+        * \param omegay y方向角速率
+        */
+        void Control(F32 omegax,F32 omegay);
+};
+#endif // CRADLECONTROL_H_INCLUDED
---
+++ ImageAquaire/camera.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "itrbase.h"
+using itr_math::Matrix;
+
+class Camera
+{
+    public:
+        /** Default constructor */
+        Camera();
+        void Init(int Width,int Height);
+        /**
+        * \brief 获取一帧图像
+        * \param 输出的灰度图像
+        */
+        void FetchFrame(Matrix &img);
+
+        void Stop();
+    protected:
+    private:
+};
+
+#endif // CAMERA_H
