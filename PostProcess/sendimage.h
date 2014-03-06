efab6fe386e51878bdd61934bd2f5fe9f983d4e4
ghdawn <ghdawn31@gmail.com>
2014-03-06 21:30:36 +0800

    增加了对itrbase的引用

--- PostProcess/sendimage.h
+++ PostProcess/sendimage.h
@@ -2,6 +2,7 @@
 #define SENDIMAGE_H
 
 #include <QtGui>
+#include "itrbase.h"
 
 class SendImage : public QWidget
 {
@@ -9,7 +10,8 @@ class SendImage : public QWidget
 public:
     SendImage(QWidget *parent=0);
     ~SendImage();
-
+    void Init(int width,int height);
+    void process(U8* inputimg,RectangleF rect,string Info,U8* outputimg);
 private:
 
 private slots:
