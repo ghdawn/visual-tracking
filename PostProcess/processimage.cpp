d42e7a73e7418f9e0e457c850a72225a59c16c83
张以成 <zycet@126.com>
2014-03-07 13:06:38 +0800

    修改processimg

--- PostProcess/processimage.cpp
+++ PostProcess/processimage.cpp
@@ -23,12 +23,7 @@ void ProcessImage::process(U8* inputimg,RectangleF rect,string Info,U8* outputim
     int i;
     int ViewX = 10;
     int ViewY = 10;
-    unsigned char *pSrc=new unsigned char[width*height];
-    for(i=0; i<width*height; i++)
-    {
-        pSrc[i] = 255;
-    }
-    QImage image = QImage(pSrc, width, height, QImage::Format_RGB32);
+    QImage image = QImage(inputimg, width, height, QImage::Format_RGB32);
     QPixmap pix(width,height);
     QPainter p(&pix);
     p.drawImage(QPoint(0, 0), image);
@@ -40,7 +35,7 @@ void ProcessImage::process(U8* inputimg,RectangleF rect,string Info,U8* outputim
     unsigned char * p_bits=img.bits();
     for(i=0; i < width*height; i++)
     {
-        pSrc[i] = p_bits[i];
+        outputimg[i] = p_bits[i];
     }
 
 }
