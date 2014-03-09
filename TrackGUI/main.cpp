9e7d98787cc77825ff853b0f1a3e09b2744dfa48
ghdawn <ghdawn31@gmail.com>
2014-03-09 15:54:02 +0800

    合并版本

--- TrackGUI/main.cpp
+++ TrackGUI/main.cpp
@@ -1,4 +1,68 @@
-#include<QApplication>
+#include <QApplication>
+#include "serialport.h"
+#include "processimage.h"
+
+int main()
+{
+
+}
+/*#include <QtGui/QApplication>
+#include <QTextCodec>
+#include "widget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("gb2312"));
+    Widget w;
+    w.show();
+    return a.exec();
+}
+#include "udpserver.h"
+#include "udpclient.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    UdpServer *server = new UdpServer;
+    UdpClient *client = new UdpClient;
+    server->show();
+    client->show();
+    return app.exec();
+}
+
+#include <QtGui/QApplication>
+#include "mainwindow.h"
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QMutex mutex;
+    //Thread thread;
+
+    //thread.start();
+    Thread thread1("Thread1");
+    thread1.mutex=&mutex;
+    thread1.start();
+    Thread thread2("Thread2");
+    thread2.mutex=&mutex;
+    thread2.start();
+
+    return app.exec();
+}
+#include <QtGui/QApplication>
+#include "mainwindow.h"
+
+int main(int argc,char *argv[]){
+   QApplication a(argc,argv);
+   MainWindow window;
+   window.show();
+   return a.exec();
+}*/
+// 用来测试串口的主程序
+/*#include<QApplication>
 #include"mainwindow.h"
 
 int main(int argc,char *argv[])
@@ -7,7 +71,9 @@ int main(int argc,char *argv[])
     MainWindow mw;
     mw.show();
     return app.exec();
-}
+}*/
+
+//用来测试图像处理的主程序
 /*#include <QtGui>
 #include "processimage.h"
 
--- TrackGUI/mainwindow.h
+++ TrackGUI/mainwindow.h
@@ -1,4 +1,69 @@
-#ifndef MAINWINDOW_H
+#include <iostream>
+#include <QApplication>
+#include <QThread>
+#include <QString>
+#include <QMutex>
+
+class Thread : public QThread
+{
+public:
+    Thread(QString name = "")
+    {
+        stopped = false;
+        this->name = name;
+    }
+
+    void run()
+    {
+        while (!stopped)
+        {
+            mutex->lock();
+            if (stopped)
+            {
+                stopped = false;
+                mutex->unlock();
+                break;
+            }
+
+            std::cout << "In " << name.toStdString() << "'s run()." << std::endl;
+            mutex->unlock();
+            QThread::msleep(4000);
+            qDebug("Threadlock Succeed!!");
+        }
+        qDebug("end!");
+    }
+
+    void stop()
+    {
+        mutex->lock();
+        stopped = true;
+        mutex->unlock();
+    }
+    QMutex* mutex;
+private:
+    volatile bool stopped;
+    QString name;
+
+};
+//用来测试多线程的窗口
+/*#ifndef MINWINDOW_H_
+#define MINWINDOW_H_
+
+#include <QtGui/QWidget>
+#include "mythread.h"
+#include <QtGui/QPushButton>
+
+class MainWindow : public QWidget
+{
+public:
+      MainWindow(QWidget *parent = 0);
+      ~MainWindow();
+      MyThread *thread;
+      QPushButton *pb;
+};
+#endif;*/
+//用来测试串口的窗口界面
+/*#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include<QtGui>
@@ -19,4 +84,4 @@ public slots:
 private:
     Thread *yy;
 };
-#endif
+#endif*/
