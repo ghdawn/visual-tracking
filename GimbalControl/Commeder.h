e7f59ca8d6a04db739954d0c0a53952ca58566dd
ghdawn <ghdawn31@gmail.com>
2014-03-28 23:20:50 +0800

    Merge branch 'master' of https://github.com/ghdawn/visual-tracking

---
+++ GimbalControl/Commeder.h
@@ -0,0 +1,21 @@
+#ifndef COMMEDER_H
+#define COMMEDER_H
+#include "itrbase.h"
+
+class Commeder
+{
+    public:
+        Commeder();
+        virtual ~Commeder();
+        //mo
+        //pa
+        //bg
+        S32 com_MO(U8* buffer,S32 para);
+        S32 com_PA(U8* buffer,F32 angle);
+        S32 com_BG(U8* buffer);
+    protected:
+    private:
+
+};
+
+#endif // COMMEDER_H
