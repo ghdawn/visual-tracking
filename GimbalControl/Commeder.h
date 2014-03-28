9989ac046ecc889a1f9429e8132a486a867a4fd3
张以成 <guanqp@126.com>
2014-03-28 23:20:14 +0800

    完善gimbol control。添加st,jv.

--- GimbalControl/Commeder.h
+++ GimbalControl/Commeder.h
@@ -9,10 +9,12 @@ class Commeder
         virtual ~Commeder();
         //mo
         //pa
-        //bg
+        //bg stop jv
         S32 com_MO(U8* buffer,S32 para);
         S32 com_PA(U8* buffer,F32 angle);
         S32 com_BG(U8* buffer);
+        S32 com_ST(U8* buffer);
+        S32 com_JV(U8* buffer,F32 speed);
     protected:
     private:
 
