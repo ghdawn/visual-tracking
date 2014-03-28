01f69ff37dc1b2dda7c5a39f2d36a9beaa1f8848
ghdawn <ghdawn31@gmail.com>
2014-03-28 23:22:17 +0800

    增加了新的文件

--- GimbalControl/Commeder.cpp
+++ GimbalControl/Commeder.cpp
@@ -18,10 +18,14 @@ S32 Commeder::com_MO(U8* buffer,S32 para)
     {
         return -1;
     }
+<<<<<<< HEAD
 
     sprintf((char*)buffer,"MO=%d",para);
 
     return 5;
+=======
+    return (sprintf((char*)buffer,"MO=%d",para));
+>>>>>>> 9989ac046ecc889a1f9429e8132a486a867a4fd3
 }
 S32 Commeder::com_PA(U8* buffer,F32 angle)
 {
@@ -29,6 +33,7 @@ S32 Commeder::com_PA(U8* buffer,F32 angle)
     {
         return -1;
     }
+<<<<<<< HEAD
     F32 length=0;
     F32 threadnum=(angle/360.0*tread_max);
     itr_math::NumericalObj->Log10(threadnum,length);
@@ -44,4 +49,27 @@ S32 Commeder::com_BG(U8* buffer)
     sprintf((char*)buffer,"BG");
 
     return (2);
+=======
+    S32 threadnum=(S32)(angle/360.0*tread_max);
+
+    return (sprintf((char*)buffer,"PA=%d",threadnum));
+}
+S32 Commeder::com_JV(U8* buffer,F32 speed)
+{
+//    if(angle<0||angle>360)
+//    {
+//        return -1;
+//    }
+    S32 threadnum=(S32)(speed/360.0*tread_max);
+
+    return (sprintf((char*)buffer,"JV=%d",threadnum));
+}
+S32 Commeder::com_BG(U8* buffer)
+{
+    return (sprintf((char*)buffer,"BG"));
+}
+S32 Commeder::com_ST(U8* buffer)
+{
+    return (sprintf((char*)buffer,"ST"));
+>>>>>>> 9989ac046ecc889a1f9429e8132a486a867a4fd3
 }
--- GimbalControl/Commeder.h
+++ GimbalControl/Commeder.h
@@ -9,10 +9,19 @@ class Commeder
         virtual ~Commeder();
         //mo
         //pa
+<<<<<<< HEAD
         //bg
         S32 com_MO(U8* buffer,S32 para);
         S32 com_PA(U8* buffer,F32 angle);
         S32 com_BG(U8* buffer);
+=======
+        //bg stop jv
+        S32 com_MO(U8* buffer,S32 para);
+        S32 com_PA(U8* buffer,F32 angle);
+        S32 com_BG(U8* buffer);
+        S32 com_ST(U8* buffer);
+        S32 com_JV(U8* buffer,F32 speed);
+>>>>>>> 9989ac046ecc889a1f9429e8132a486a867a4fd3
     protected:
     private:
 
