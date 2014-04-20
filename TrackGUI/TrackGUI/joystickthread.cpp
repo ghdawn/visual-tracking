4783c2a7e68d72a488767009e9f45cd788fc740d
张以成 <zycet@126.com>
2014-04-20 16:54:45 +0800

    修改摇杆线程添加矩形框处理

--- TrackGUI/TrackGUI/joystickthread.cpp
+++ TrackGUI/TrackGUI/joystickthread.cpp
@@ -27,13 +27,65 @@ void JoyStickThread::run()
         if(joystick.GetButtonStatus(0))
         {
             core->Manual();
-            core->posInit.Height = core->posInit.Height + 0.2*joystick.GetAxisValue(2);
-            core->posInit.Width = core->posInit.Width + 0.2*joystick.GetAxisValue(3);
-            gimbal.Control(joystick.GetAxisValue(0),joystick.GetAxisValue(1));
+
         }
         else if(joystick.GetButtonStatus(1))
         {
             core->BeginTrack();            
+        }
+        if(core->Tracking)
+        {
+            for(S32 i=0; i<axiscount; i++)
+            {
+                axisvalue[i]=JoyStickPro.GetAxisValue(i);
+                if(axisvalue[4]>0.5||axisvalue[4]<-0.5)
+                {
+                    core->posInit.Height = core->posInit.Height*(1-axisvalue[4]*0.001);
+
+                }
+
+                if(axisvalue[3]>0.5||axisvalue[3]<-0.5)
+                {
+                    core->posInit.Width = core->posInit.Width*(1+axisvalue[3]*0.001);
+
+                }
+            }
+
+//            if(axisvalue[2]>0.8)
+//            {
+//                core->posInit.Width = 1.2*core->posInit.Width;
+//            }
+//            else if(axisvalue[2]>0.6&&axisvalue[2]<0.8)
+//            {
+//                core->posInit.Width = 1.1*core->posInit.Width;
+//            }
+//            else if(axisvalue[2]>-0.8&&axisvalue[2]<-0.6)
+//            {
+//                core->posInit.Width = 0.9*core->posInit.Width;
+//            }
+//            else if(axisvalue[2]<-0.8)
+//            {
+//                core->posInit.Width = 0.8*core->posInit.Width;
+//            }
+
+//            if(axisvalue[3]>0.8)
+//            {
+//                core->posInit.Height = 1.2*core->posInit.Height;
+//            }
+//            else if(axisvalue[3]>0.6&&axisvalue[3]<0.8)
+//            {
+//                core->posInit.Height = 1.1*core->posInit.Height;
+//            }
+//            else if(axisvalue[3]>-0.8&&axisvalue[3]<-0.6)
+//            {
+//                core->posInit.Height = 0.9*core->posInit.Height;
+//            }
+//            else if(axisvalue[3]<-0.8)
+//            {
+//                core->posInit.Height = 0.8*core->posInit.Height;
+//            }
+
+           // gimbal.Control(joystick.GetAxisValue(0),joystick.GetAxisValue(1));
         }
         msleep(100);
     }
