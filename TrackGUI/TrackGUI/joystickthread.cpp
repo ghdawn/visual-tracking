099ea99cffb34404c74331d7de35c70961e9e6ed
张以成 <zycet@126.com>
2014-04-23 16:23:13 +0800

    摇杆线程

--- TrackGUI/TrackGUI/joystickthread.cpp
+++ TrackGUI/TrackGUI/joystickthread.cpp
@@ -23,22 +23,23 @@ void JoyStickThread::run()
 {
     while (isopen&&!stopped)
     {
+        U8 *buffer;
         joystick.Update();
         S32 axiscount=0;
         axiscount=joystick.GetAxisCount();
         F32 axisvalue[30]={0};
+
         if(joystick.GetButtonStatus(0))
         {
             core->Manual();
-
         }
         else if(joystick.GetButtonStatus(1))
         {
             core->BeginTrack();            
         }
-        if(core->Tracking)
-        {
 
+        if(!core->Tracking)
+        {
             for(S32 i=0; i<axiscount; i++)
             {
                 axisvalue[i]=joystick.GetAxisValue(i);
@@ -54,41 +55,17 @@ void JoyStickThread::run()
                 core->posInit.Width = core->posInit.Width*(1+axisvalue[3]*0.001);
             }
 
-//            if(axisvalue[2]>0.8)
-//            {
-//                core->posInit.Width = 1.2*core->posInit.Width;
-//            }
-//            else if(axisvalue[2]>0.6&&axisvalue[2]<0.8)
-//            {
-//                core->posInit.Width = 1.1*core->posInit.Width;
-//            }
-//            else if(axisvalue[2]>-0.8&&axisvalue[2]<-0.6)
-//            {
-//                core->posInit.Width = 0.9*core->posInit.Width;
-//            }
-//            else if(axisvalue[2]<-0.8)
-//            {
-//                core->posInit.Width = 0.8*core->posInit.Width;
-//            }
+            if(axisvalue[0]>0.5||axisvalue[0]<-0.5)
+            {
+                ASF32(&buffer[1]) = axisvalue[0];
+            }
 
-//            if(axisvalue[3]>0.8)
-//            {
-//                core->posInit.Height = 1.2*core->posInit.Height;
-//            }
-//            else if(axisvalue[3]>0.6&&axisvalue[3]<0.8)
-//            {
-//                core->posInit.Height = 1.1*core->posInit.Height;
-//            }
-//            else if(axisvalue[3]>-0.8&&axisvalue[3]<-0.6)
-//            {
-//                core->posInit.Height = 0.9*core->posInit.Height;
-//            }
-//            else if(axisvalue[3]<-0.8)
-//            {
-//                core->posInit.Height = 0.8*core->posInit.Height;
-//            }
+            if(axisvalue[1]>0.5||axisvalue[1]<-0.5)
+            {
+                ASF32(&buffer[2]) = axisvalue[1];
+            }
 
-           // gimbal.Control(joystick.GetAxisValue(0),joystick.GetAxisValue(1));
+            gimbal.Control( ASF32(&buffer[1]), ASF32(&buffer[2]));
         }
         msleep(100);
     }
